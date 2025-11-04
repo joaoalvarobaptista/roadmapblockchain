22064ad890f8f4aad5b26d29dc41a488c0fe98a5
Joao Alvaro de Souza Baptista <joaoalvaro@hotmail.com>
2025-11-04 11:53:02 -0300

    Estudando JavaScript com Bonieki Lacerda

---
+++ Curso Bonieki/JavaScript/modulo01/script.js
@@ -0,0 +1,12 @@
+var x = 0.1;
+var y = 0.2;
+var soma = x + y;
+
+alert(soma);
+
+var hora = 13;
+if(hora < 12){
+    console.log('Bom dia!');
+}else{
+    console.log('Não está de manha!');
+}
---
+++ Curso Bonieki/JavaScript/modulo02/aula02.js
@@ -0,0 +1,28 @@
+function verde(){
+    document.querySelector('#exemplo').classList.add('verde');
+    document.querySelector('#exemplo').classList.remove('vermelho');
+    document.querySelector('#exemplo').classList.remove('azul');
+}
+
+function vermelho(){
+    document.querySelector('#exemplo').classList.add('vermelho');
+    document.querySelector('#exemplo').classList.remove('verde');
+    document.querySelector('#exemplo').classList.remove('azul');
+}
+
+function azul(){
+    document.querySelector('#exemplo').classList.add('azul');
+    document.querySelector('#exemplo').classList.remove('verde');
+    document.querySelector('#exemplo').classList.remove('vermelho');
+}
+
+function trocar(){
+    if (document.querySelector('#exemplo').classList.contains('preto')){
+        document.querySelector('#exemplo').classList.remove('preto');
+        document.querySelector('#exemplo').classList.add('verde');
+    } else {
+        document.querySelector('#exemplo').classList.remove('verde')
+        document.querySelector('#exemplo').classList.add('preto');
+    }
+        
+}
