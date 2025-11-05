965d23d06f420fac906d5e938293d595be2c5860
Joao Alvaro de Souza Baptista <joaoalvaro@hotmail.com>
2025-11-05 13:38:25 -0300

    Estudo de 05112025

---
+++ Curso Bonieki/JavaScript/modulo02/aula10.js
@@ -0,0 +1,9 @@
+function trocar(){
+    document.getElementById('title').innerHTML = 'Funcionou';
+    document.getElementById('fieldset').value = 'Funcinou também';
+}
+
+function somar(x, y){
+    let soma = x + y;
+    return soma;
+}
